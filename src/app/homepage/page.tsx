c131367f981a6af774fea50371d48f9792897b92
peculiar <pecuiheubammadu1st@gmail.com>
2024-11-22 15:21:23 +0100

    created the homepage

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Homepage() {
+  return (
+    <div>
+        <h1>
+            homepage
+        </h1>
+        <p>
+            This is a homepage.
+        </p>
+    </div>
+  )
+}
